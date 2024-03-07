743fe298b1a64cc208e63e88fe6415958867b550
masfaghori <161416552+masfaghori@users.noreply.github.com>
2024-03-07 13:48:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,52 @@
+"use strict";
+// "THIS IS MY ASSIGNMENT OF TODAY'S CLASS!"
+// 1.EXAMPLE OF VARIABLE NAMING CONVENTION:
+// "camelCase";
+var userProfile = "Masfa";
+console.log(userProfile);
+// "snake_case":
+var user_profile = "Abdullah";
+console.log(user_profile);
+// "PascalCase":
+var UserProfile = "Aliza";
+console.log(UserProfile);
+// 2.EXAMPLE OF DATATYPES:
+// "STRING":
+var countryName = "Pakistan";
+console.log(countryName);
+var cityName = "Karachi";
+console.log("I live in " + cityName, "which is the city of " + countryName);
+// "NUMBERE":
+var rollNumber = "100";
+console.log(rollNumber);
+var a = 5;
+var b = 2;
+console.log("a =", a, "and b =", b);
+console.log("a + b =", a + b);
+console.log("a / b =", a / b);
+//"BOOLEAN":
+var isPositive = "true";
+console.log(isPositive);
+var submitted = "false";
+console.log(submitted);
+// "UNDEFINED":
+var anyWord;
+console.log(anyWord);
+// "NULL";
+var object = "null";
+console.log(object);
+// "ARRAYS":
+var userList = ["Abdullah", "Aliza", "Maryam", "Masfa", "Jahan", "Imama"];
+var newUserList = userList;
+console.log(newUserList);
+console.log(userList[0]);
+console.log(userList[0], userList[2]);
+var oneArray = userList[0];
+console.log(oneArray);
+var userNameAndRollNumber = ["Hamza", "Khan", "Faris", "YAMNA", "GHORI", 15, 45, 66];
+console.log(userNameAndRollNumber[0], userNameAndRollNumber[6]);
+userList[1] = "Masfa";
+console.log(userList);
+// "BIGINT":
+var bigNumber = 6734239041;
+console.log(bigNumber);
---
+++ app.ts
@@ -0,0 +1,54 @@
+// "THIS IS MY ASSIGNMENT OF TODAY'S CLASS!"
+// 1.EXAMPLE OF VARIABLE NAMING CONVENTION:
+// "camelCase";
+let userProfile = "Masfa";
+console.log(userProfile);
+// "snake_case":
+let user_profile ="Abdullah";
+console.log(user_profile);
+// "PascalCase":
+let UserProfile = "Aliza"
+console.log(UserProfile);
+// 2.EXAMPLE OF DATATYPES:
+// "STRING":
+let countryName = "Pakistan";
+console.log(countryName);
+let cityName = "Karachi";
+console.log("I live in " + cityName, "which is the city of " + countryName);
+// "NUMBERE":
+let rollNumber = "100"
+console.log(rollNumber);
+let a = 5
+let b = 2
+console.log("a =", a, "and b =", b);
+console.log("a + b =", a + b);
+console.log("a / b =", a / b);
+//"BOOLEAN":
+let isPositive = "true";
+console.log(isPositive);
+let submitted = "false";
+console.log(submitted);
+// "UNDEFINED":
+let anyWord;
+console.log(anyWord);
+// "NULL";
+let object = "null";
+console.log(object);
+// "ARRAYS":
+var userList = ["Abdullah", "Aliza", "Maryam", "Masfa", "Jahan", "Imama"];
+var newUserList = userList;
+console.log(newUserList);
+console.log(userList[0]);
+console.log(userList[0], userList[2]);
+var oneArray = userList[0];
+console.log(oneArray);
+var userNameAndRollNumber = ["Hamza", "Khan", "Faris", "YAMNA", "GHORI", 15, 45, 66];
+console.log(userNameAndRollNumber[0], userNameAndRollNumber[6]);
+userList[1] = "Masfa";
+console.log(userList);
+// "BIGINT":
+let bigNumber = 6734239041;
+console.log(bigNumber);
+
+
+
